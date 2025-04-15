fee21029a3aab64b8475b002f913c69231076880
Liza <elizavetamihel9@gmail.com>
2025-04-15 23:31:56 +0300

    Добавлены тесты на JUnit4 для CashbackHackService

---
+++ src/test/java/ru/netology/service/CashbackTest.java
@@ -0,0 +1,22 @@
+package ru.netology.service;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+public class CashbackTest {
+    @Test
+    public void shouldReturn100IfAmountIs900() {
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(900);
+        int expected = 100;
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldReturn0IfAmountIs1000() {
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(1000);
+        int expected = 0;
+        Assert.assertEquals(expected, actual); // тут тест тоже упадёт
+    }
+}
